30c1d06eeb5ee794042bc6b2b73ac951487b560e
ziu03 <57125940+sonjiwoo031105@users.noreply.github.com>
2025-06-09 23:30:15 +0900

    move the global css location

--- app/(home)/error.tsx
+++ app/(home)/error.tsx
@@ -6,7 +6,7 @@ export default function Error(
   return (
     <div className="text-center text-red-500">
       <p>문제가 발생했어요: {error.message}</p>
-      <button onClick={reset} className="mt-4 bg-gray-200 p-2 rounded">다시 시도하기</button>
+      <button onClick={reset} className="mt-4 bg-gray-200 p-2 rounded cursor-pointer">다시 시도하기</button>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../styles/globals.css";
 import CustomSessionProvider from "@/components/SessionProvider";
 
 export const metadata: Metadata = {
